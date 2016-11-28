0f44d6dcd64741c6aeb3bf81ea939d3a483318fd
qiaopenghong <278058342@qq.com>
2016-11-28 20:48:21 +0800

    Revert "二分法"
    This reverts commit 1066c27b85d0db52e1e5629dcaba8351c1e74bdc.

--- BinarySearchTest.java
+++ BinarySearchTest.java
@@ -1,85 +1,17 @@
 
 public class BinarySearchTest {
   
-	int MySalary=14000;
-    double percent=0.1f;
-
-    public void textSum(){
-        double SUM = MySalary * percent*2;
-        System.out.printf("月公积金----->"+SUM);
-    }
-
-    //递归
-    int doSum(int n){
-        if(n==100){
-            return n;
-        }
-        return n+doSum(n+1);
-    }
-
-
-    public static void main(String[] args) {
-        //算法
-            new BinarySearchTest().textSum();
-
-        //二分发
-        int[] nums = { 2, 5, 11, 3, 22, 6, 91, 13, 35, 4 };
-       //对数组进行排序
-//        QuickSort.sort(nums, 0, nums.length-1);
-
-        int find =BinarySearchTest.search(nums,5);
-
-        if (find != -1) {
-            System.out.println("\n找到数值于索引" + find);
-        } else {
-            System.out.println("找不到数值");
-        }
-
-
-        //1--100递归算法
-        new BinarySearchTest().doSum(1);
-
-        //1--100递归算法
-        System.out.printf(String.valueOf("----------->"+dg(100)));
-
-        //1--100递归算法
-        new BinarySearchTest().doSum(1);
-
-    }
-
-
-
-    //1--100递归算法
-    static int dg(int i) {     /*定义变量 */
-        int sum;
-        if (i == 1)         /*假设条件*/
-            return 1;
-        else
-            sum = i + dg(i - 1);     /*1~100的和的表达式*/
-        return sum;              /*返回结果*/
-    }
-
-
-
-    //二分法
-    public static int search(int[] nums, int num) {
-        int low = 0;
-        int high = nums.length - 1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            //与中间值比较确定在左边还是右边区间,以调整区域
-            if (num > nums[mid]) {
-                low = mid + 1;
-            } else if (num < nums[mid]) {
-                high = mid - 1;
-            } else {
-                return mid;
-            }
-        }
-
-        return -1;
-    }
-
+	    public void testSearch()  
+	    {  
+	        BinarySearch bs=new BinarySearch();  
+	        int[] sortedData={1,2,3,4,5,6,6,7,8,8,9,10};  
+	        int findValue=9;  
+	        int length=sortedData.length;  
+	          
+	        int pos=bs.searchRecursive(sortedData, 0, length-1, findValue);  
+	        System.out.println("Recursice:"+findValue+" found in pos "+pos+";count:"+bs.getrCount());  
+	        int pos2=bs.searchLoop(sortedData, findValue);  
+	          
+	        System.out.println("Loop:"+findValue+" found in pos "+pos+";count:"+bs.getlCount());  
+	    }  
 }
